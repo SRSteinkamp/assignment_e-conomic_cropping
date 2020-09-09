96898efac5449b496b5f019401bd6f90eca44fee
SRSteinkamp <simon.steinkamp@googlemail.com>
2020-09-09 14:33:30 +0200

    tests for iou_wrapper, smokescrean datagenerator

---
+++ tests/test_data_gen.py
@@ -0,0 +1,19 @@
+from cropping_lib.model_parts import DataGenerator, iou_wrapper
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+
+
+def test_smoke_DataGenerator():
+    # check if DataGenerator can be created
+    df = pd.DataFrame(np.arange(10))
+    DataGenerator(df)
+
+
+def test_iou_wrapper():
+    # Test if iou_wrapper provides results:
+    # TopLeftX,TopLeftY,TopRightX,TopRightY,BottomRightX,BottomRightY,BottomLeftX,BottomLeftY
+    boxes1 = tf.constant([[0.1, 0.9, 0.9, 0.9, 0.9, 0.1, 0.1, 0.1],
+                          [0.1, 0.9, 0.9, 0.9, 0.9, 0.1, 0.1, 0.1]])
+
+    assert np.isclose(0, np.sum(iou_wrapper(boxes1, boxes1)))
