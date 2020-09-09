2b4213fe11e9d88ba3deba95e4fe0c691d650d2e
SRSteinkamp <simon.steinkamp@googlemail.com>
2020-09-09 14:35:38 +0200

    cleaning training a bit

--- train_model.py
+++ train_model.py
@@ -4,12 +4,16 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 from PIL import Image
-from cropping_lib.model_parts import DataGenerator
+from cropping_lib.model_parts import DataGenerator, iou_wrapper
 # %%
+LR = 0.001
+EPOCHS = 100
 train_csv = pd.read_csv('data/train.csv')
 valid_csv = pd.read_csv('data/validation.csv')
-# %%
+
+# Data generators from training
 train_generator = DataGenerator(train_csv, batch_size=20)
+valid_generator = DataGenerator(valid_csv, batch_size=1, shuffle=False)
 # %% If no model exists: Initiate model
 base = keras.applications.MobileNetV2(input_shape=(224,224,3),
                                       include_top=False, weights='imagenet')
@@ -20,15 +24,27 @@ model = keras.Sequential([ base,
                            keras.layers.Dropout(0.5),
                            keras.layers.Dense(8, activation='sigmoid')])
 # %% Else: Load model, pass learning rate via CLI, epochs, callbacks:
-# Load model
+# Callbacks:
+cb_logging = tf.keras.callbacks.CSVLogger('models/log.csv', separator=",", append=False)
+
+cb_checkpoint = tf.keras.callbacks.ModelCheckpoint(
+    filepath='models',
+    save_weights_only=False,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+cb_earlystopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+
+cb_reduceLR = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', patience=5)
 
 # Edit model name:
-optimizer = keras.optimizers.Adam(learning_rate=0.001)
-model.compile(optimizer=optimizer, loss = 'mean_squared_error')
+optimizer = keras.optimizers.Adam(learning_rate=LR)
+model.compile(optimizer=optimizer, loss = iou_wrapper)
 # %%
-model.fit(train_generator, epochs=5)
-# %% Print out validation score:
-valid_generator = DataGenerator(valid_csv, batch_size=1, shuffle=False)
-model.evaluate(valid_generator)
+model.fit(train_generator, validation_data = valid_generator,
+          epochs=EPOCHS,
+          callbacks=[cb_logging, cb_checkpoint,
+                     cb_earlystopping, cb_reduceLR])
 
 # %%
