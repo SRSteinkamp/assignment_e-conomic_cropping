27c01c94a8014a355f8d35971e439e953ebb6c8a
SR Steinkamp <simon.steinkamp@googlemail.com>
2020-09-13 17:40:32 +0200

    customnet v0 - IOU 0.56

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -5,23 +5,23 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow import keras
 from cropping_lib.utils import check_working_dir
-from cropping_lib.model_parts import DataGenerator, IOU_TwoBox
+from cropping_lib.model_parts import DataGenerator, IOU_TwoBox, IOU_LargeBox
 from cropping_lib.model_parts import build_model
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 # %% Training settings
-LR = 0.001  # Learning Rate
+LR = 0.005  # Learning Rate
 EPOCHS = 5000  # Number of Epochs
 BASEPATH = check_working_dir(os.path.realpath(__file__))
 DATAPATH = BASEPATH + '/data/'  # Data Location
-MODELPATH = BASEPATH + '/model/mobilenetv2/'  # The model to load
-MODELNAME = BASEPATH + '/model/mobilenetv2/'  # The model to save
+MODELPATH = BASEPATH + '/model/customnetv0/'  # The model to load
+MODELNAME = BASEPATH + '/model/customnetv0/'  # The model to save
 train_csv = pd.read_csv(f'{DATAPATH}/train.csv')
 valid_csv = pd.read_csv(f'{DATAPATH}/validation.csv')
 
 # Data generators for training and validation
-train_generator = DataGenerator(train_csv, batch_size=20, shuffle=True)
+train_generator = DataGenerator(train_csv, batch_size=10, shuffle=True)
 valid_generator = DataGenerator(valid_csv, batch_size=1, shuffle=False)
 
 # If the model does not exist: Initiate model, else reload previous model
@@ -40,14 +40,14 @@ cb_checkpoint = ModelCheckpoint(filepath=MODELPATH,
                                 mode='min',
                                 save_best_only=True)
 # Early stopping to prevent overfitting
-cb_earlystopping = EarlyStopping(monitor='val_loss', patience=20)
+cb_earlystopping = EarlyStopping(monitor='val_loss', patience=15)
 # There is more fancy stuff out there, let's just leave it with this
 cb_reduceLR = ReduceLROnPlateau(monitor='val_loss', patience=5)
 
 # Compile model
 optimizer = keras.optimizers.Adam(learning_rate=LR)
 model.compile(optimizer=optimizer, loss=IOU_TwoBox(),
-              metrics=['mean_absolute_error'])
+              metrics=[IOU_LargeBox(), 'mean_absolute_error'])
 
 # Fit
 history = model.fit(train_generator, validation_data=valid_generator,
